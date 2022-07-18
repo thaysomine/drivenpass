8b0c1dd83578fc3a08df337d94c17d6701b195d7
Thays <thaysomine@gmail.com>
2022-07-18 16:47:17 -0300

    feat: card validation

--- src/middlewares/joiValidation.ts
+++ src/middlewares/joiValidation.ts
@@ -4,6 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 import * as userRepository from '../repositories/userRepository.js';
 import * as credentialRepository from '../repositories/credentialRepository.js';
 import * as noteRepository from '../repositories/noteRepository.js';
+import * as cardRepository from '../repositories/cardRepository.js';
 
 export async function validateUser(req: Request, res: Response, next: NextFunction) {
     const data: userRepository.UserInsertData = req.body;
@@ -34,6 +35,22 @@ export async function validateNote(req: Request, res: Response, next: NextFuncti
     const schema = joi.object({
         title: joi.string().max(50).required(),
         content: joi.string().max(1000).required()
+    });
+    const { error } = schema.validate(data);
+    if (error) throw new Error(error.message);
+    next();
+}
+
+export async function validateCard(req: Request, res: Response, next: NextFunction) {
+    const data: cardRepository.cardInsertData = req.body;
+    const schema = joi.object({
+        title: joi.string().required(),
+        number: joi.string().required(),
+        cvv: joi.string().required(),
+        password: joi.string().required(),
+        expirationDate: joi.string().required(),
+        isVirtual: joi.boolean().required(),
+        type: joi.string().required()
     });
     const { error } = schema.validate(data);
     if (error) throw new Error(error.message);
--- src/routes/cardsRouter.ts
+++ src/routes/cardsRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { createCard, getCards, getCardById, deleteCard } from "../controllers/cardsController.js";
+import { validateCard } from "../middlewares/joiValidation.js";
 
 const cardsRouter = Router();
 
-cardsRouter.post("/create/card", createCard);
+cardsRouter.post("/create/card", validateCard, createCard);
 cardsRouter.get("/card", getCards);
 cardsRouter.get("/card/:id", getCardById);
 cardsRouter.delete("/card/:id", deleteCard);
